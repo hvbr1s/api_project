40ffe6f2cf11e53a4ffa19ac893bd20060456616
Dan JAHENY <dan@LAPTOP-LIL83ON6>
2023-09-30 16:22:40 +0200

    moar code

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { generateApiKey } = require('generate-api-key')
 const app = express()
 const PORT = 8888
 require('dotenv').config()
@@ -12,20 +13,20 @@ const DOMAIN = `http://localhost:${PORT}/`
 app.use(express.static('public'))
 
 // routes
-app.post('/create-checkout-session/:product', (req, res) =>{
+app.post('/create-checkout-session/:product', async (req, res) =>{
 
     const { product } = req.params
     let mode, price_ID, line_items
 
     if (product === 'sub'){
-        price_ID = ''
+        price_ID = 'price_1Nw3eDK2BEcUysLFZbUnSpKD'
         mode = 'subscription'
         line_items ={
             price: price_ID,
         }
 
     } else if (product === 'pre') {
-        price_ID=''
+        price_ID='price_1Nw3pUK2BEcUysLFWJ1BgWJw'
         mode= 'payment'
         line_items={
             price: price_ID,
@@ -39,8 +40,32 @@ app.post('/create-checkout-session/:product', (req, res) =>{
 
     }
 
-}
-)
+    const newAPIKey = generateApiKey()
+    const customer = await stripe.customers.create({
+        metadata: {
+            APIkey: newAPIKey,
+            payment_type: product
+        }
+    })
+
+    const stripeCustomerId = customer.id
+    const session = await stripe.checkout.sessions.create({
+        customer: stripeCustomerId,
+        metadata: {
+            APIkey: newAPIKey
+
+        },
+        line_items: line_items,
+        mode: mode,
+        success_url: `${DOMAIN}/success.html?api_key=${newAPIKey}`,
+        cancel_url: `${DOMAIN}/cancel.html`,
+    })
+
+    // create firebase record
+
+    //use webhook to access firebase entry for api key
+    res.redirect(303, session.url)
+})
 
 
 app.listen(PORT, () => console.log(`Server has started on port: ${DOMAIN}`))
